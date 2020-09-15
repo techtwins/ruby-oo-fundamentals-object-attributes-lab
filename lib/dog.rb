8fa5a7e2f2eb3e9fa1b2e685f8f69188f1c04a69
techtwins <gshevaughn@gmail.com>
2020-09-14 21:07:20 -0400

    tests are all passing

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+    def name
+        @name
+    end
+    
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def breed
+        @breed
+    end
+    
+    def breed=(breed_type)
+        @breed = breed_type
+    end
+end
+
+  
+#     def name
+#       @name
+#     end
+  
+#     def name=(new_name)
+#       @name = new_name
+#     end
+# end
